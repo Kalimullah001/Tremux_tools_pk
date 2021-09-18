b99f81a5764697f2b2e465a32c1a725ac60e8a00
Kalimullah001 <90949895+Kalimullah001@users.noreply.github.com>
2021-09-18 11:00:21 +0430

    Create Kalimullah.sh

---
+++ Kalimullah.sh
@@ -0,0 +1,85 @@
+echo ""
+echo ""
+echo ""
+echo "The script is  made by Kalimullah Nazir❤"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Installtion process is started" 
+echo ""
+echo ""
+echo ""
+
+pkg update -y                    
+pkg install python -y           
+pkg install python2 -y
+pkg install ruby -y 
+pkg install git -y
+pkg install screenfetch -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install figlet -y
+pkg install php -y 
+pkg install perl -y
+pkg install nmap -y
+pkg install openssh -y
+pkg install bash -y
+pkg install clang -y
+pkg install jq -y
+pkg install nano -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install fish -y
+pkg install unzip -y
+pkg install tor -y 
+pkg install sudo -y
+pkg install wget -y
+pkg install tor -y
+pkg install w3m -y
+pkg install cmatrix -y
+pkg install wcalc -y 
+pkg install openssl -y
+pkg install bmon -y
+pkg upgrade -y
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo "All basic packages and tools is installed"
+echo ""
+echo ".    □□　□□□
+□■■■□■■■■□
+□■■■■■■■■■□
+□■■■■■■■■□
+□■■■■■■■□
+　 □■■■■■□
+　　□■■■□
+　　　□□ 　
+。　　 /　　☆+
+　☆。/
+　　 /　
+　　/
+　 (　　＋☆
+　　＼　　　　。
+　　　＼
+　　　 )
+　　　/
+。　 /  Kalimullah.
+　　/　　            Nazir♡
+　　 ♡
+　 ＼●/
+　　 ■
+　　 ||
+^''^''^''^''''^''^''^''^''"
+echo ""
+echo ""
+github ; https://github.com/Kalimullah001"
+echo ""
+echo " this sceipt craeted by kalimullah Nazir👍"
+exit
